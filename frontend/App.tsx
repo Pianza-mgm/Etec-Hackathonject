670876dd1b34ccb9ab374fd0a3fe87f2d862af17
Pianza-mgm <lunium.mgm@gmail.com>
2025-04-28 22:15:09 -0300

    First part is done

---
+++ frontend/App.tsx
@@ -0,0 +1,23 @@
+import React, { StrictMode, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import axios from 'axios';
+
+function App() {
+    const HandleClick = async() => {
+        try{
+            var userData = await axios.get('http://localhost:3000/profile');
+            window.location.href = '/src/Views/Client/Main/index.html';
+        }
+        catch{
+            window.location.href = '/src/Views/LogIn/index.html';
+        }
+    }
+    return(
+        <div onClick={HandleClick}>Começar</div>
+    )
+}
+createRoot(document.getElementById('tsx-btn')!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
